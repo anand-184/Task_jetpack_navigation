437b27d42b2b9be4a5d7df7ac64d1bef47048da6
anand-184 <anandita9464@gmail.com>
2024-06-20 21:54:48 +0530

    commit

---
+++ app/src/main/java/com/anand/jetpacknavigation/BlankFragment2.kt
@@ -0,0 +1,70 @@
+package com.anand.jetpacknavigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
+import com.anand.jetpacknavigation.databinding.FragmentBlank2Binding
+
+
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+
+class BlankFragment2 : Fragment() {
+
+    private var param1: String? = null
+    private var param2: String? = null
+    var binding: FragmentBlank2Binding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentBlank2Binding.inflate(layoutInflater)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.moveTof1?.setOnClickListener {
+            findNavController().popBackStack(R.id.blankFragment2,true)
+
+
+        }
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment BlankFragment2.
+         */
+
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            BlankFragment2().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
+
+
